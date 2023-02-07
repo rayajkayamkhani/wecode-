1c16ece23803f20a0289dd16b3b692d2748b3513
rayajkayamkhani <122891600+rayajkayamkhani@users.noreply.github.com>
2023-02-07 21:05:44 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,304 @@
+// // let answer = prompt("are you ready to play game?");
+// //  if (answer){
+// //     alert("let's start the game ")
+// //  }else{
+// //     alert("sorry visit the again")
+// //  }
+   
+
+// //  let coffee = prompt("Do you drink coffee or tea? ");
+// //  if (coffee){
+// //     alert("coffee ")
+// //  }else{
+// //     alert("tea")
+// //  }
+ 
+ // // 1 answer 
+// //  let num1 = 10;
+//// let num2 = 20;
+
+// // let total = num1+num2;
+// // let thirdNumber  = total+70;
+// // console.log(thirdNumber);
+
+// // let english = prompt("What is your English marks in 10th?");
+// // let science = prompt("What is your science marks in 10th?");
+// // let math = prompt("What is your math marks in 10th?");
+// // let hindi = prompt("What is your hindi marks in 10th?");
+// // let sst = prompt("What is your sst marks in 10th?");
+// // let totelmark=  ( 100 ) ;
+// // console.log(english,science,math,hindi,sst);
+
+// // console.log(totelmark);
+
+
+// // let name = prompt("What is your name ?");
+// // let aadharNumber = prompt("What is your aaadhar number ?");
+// // let address = prompt("What is your address?");
+// // let mobileNumber = prompt("What is your mobile number?");
+
+
+// // console.log(name,aadharNumber,address,mobileNumber);
+// // 2  answer    
+// // let english = prompt("What is your English marks in 10th?");
+// // let science = prompt("What is your science marks in 10th?");
+// // let math = prompt("What is your math marks in 10th?");
+// // let hindi = prompt("What is your hindi marks in 10th?");
+// // let sst = prompt("What is your sst marks in 10th?");
+// // let totalpercent = 500;
+// // let num1 = parseInt(english);
+// // let num2 = parseInt(science);
+// // let num3 = parseInt(math);
+// // let num4 = parseInt(hindi);
+// // let num5 = parseInt(sst);
+
+// // let total = num1+num2+num3+num4+num5;
+// // console.log("Total Marks: "+total);
+
+// // let percent = total*100/totalpercent;
+// // console.log(percent+"%");
+// // 8 answer
+// let ask = prompt("What is your age ?");
+// if (ask>18) {
+//     prompt("Do you want open account? if yes then click on okay button");
+//     let name = prompt("what is your name ?");
+//     let aadahr = prompt("what is your aadhar No ?");
+//     let address = prompt("what is your address ?");
+//      let mobile = prompt("what is your mobile No ?");
+//     console.log(name,aadahr,address,mobile);
+// }
+//  else{
+//     alert("sorry visit the again")
+//  }
+
+// ///let name = confirm("what is your name")
+// console.log(name + 10);
+// console.log(name + true);
+// console.log(30+"WeCode")   
+
+
+// let name = "Adil";
+// console.log(name + 10);
+// console.log(name + true);
+// console.log(30+"WeCode");
+
+
+// let user = prompt("what is your birth year")
+// if (user > 1999 ) {
+//     console.log(Math.floor(user));
+//     console.log("In 20th Century")
+// } else {
+//     console.log("In 19th Century")
+// }
+// 5 answer
+// let option1 = prompt("what do you like tea or coffe ?")
+// let tea = "tea";
+// let cofee = "cofee";
+// if (option1 == tea) {
+//     alert("Coffe")
+// }else if(option1 == cofee) {
+//     alert("tea")
+// }
+
+// if (option2 == cofee) {
+//     alert("tea")
+// }else if(option2  == tea) {
+//     alert("Coffe")
+// }
+
+// let temperature = 20;
+
+// if (temperature<10) {
+//     console.log("very cold");
+// }else if(temperature<30 && temperature>=10){
+//     console.log("cold");
+// }else if(temperature>30 || temperature<40){
+//     console.log("hot")
+// }
+
+
+// let option = prompt("what do you want to do add,sub.mult,divide");
+// let option1 = prompt("Choose the first Number")
+// let add = option;
+// let sub = option;
+// let mult = option;
+
+// let num = parseInt(option1);
+// let option2 = prompt("Choose the second Number")
+
+// let num2 = parseInt(option2);
+
+// let total1 = num+num2;
+// let total2 = num-num2;
+// let total3 = num*num2;
+
+// let get = prompt(total1); 
+// let get1 = prompt(total2); 
+// let get2 = prompt(total3); 
+
+// if (option===add) {
+//     console.log(get)
+
+// }else if(option === sub){
+//     console.log(get1)
+// }else{
+//     console.log(get2)
+// }
+
+
+// let a = 20;
+// console.log(++a );
+
+// let name = prompt("what is your name?");
+// alert (name);
+
+// let b = 31;
+
+// if (b%2!==0) {
+//     console.log("no is odd")
+// } else {
+//     console.log("no is even")
+// }
+
+
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// let e = 5
+// let f = 6
+// let g = 7
+// let h = 8
+// let n = 9
+// let m = 10
+// console.log(++a, ++b,++c,++d,++e,++f,++g,++h,++n,++m,)
+
+// let english = prompt("What is your English marks in 10th?");
+// let science = prompt("What is your science marks in 10th?");
+// let math = prompt("What is your math marks in 10th?");
+// let hindi = prompt("What is your hindi marks in 10th?");
+// let sst = prompt("What is your sst marks in 10th?");
+// let totalpercent = 500;
+// let num1 = parseInt(english);
+// let num2 = parseInt(science);
+// let num3 = parseInt(math);
+// let num4 = parseInt(hindi);
+// let num5 = parseInt(sst);
+
+// let total = num1+num2+num3+num4+num5;
+// console.log("Total Marks: "+total);
+
+// let percent = total*100/totalpercent;
+// console.log(percent+"%");
+
+
+// let p1 = Number(prompt("What is your percentage?"));
+// let p2 = Number(prompt("What is your percentage?"));
+// let p3 = Number(prompt("What is your percentage?"));
+// let p4 = Number(prompt("What is your percentage?"));
+// let p5 = Number(prompt("What is your percentage?"));
+// let p6 = Number(prompt("What is your percentage?"));
+// let ans = Math.max(p1, p2, p3, p4, p5, p6);
+// console.log(ans);
+
+//    let ans = prompt("What do you want to peform?");
+//    let num1 = Number(prompt("Enter number 1"));
+//    let num2 = Number(prompt("Enter number 2"));
+//    if (ans === "+") {
+//    console.log(num1 + num2);
+//    } else if (ans === "-") {
+//    console.log(num1 - num2);
+//    } else if (ans === "/") {
+//    console.log(num1 / num2);
+//    } else if (ans === "_") {
+//    console.log(num1 - num2);
+//    } else {
+//    console.log("Wrong choice");
+//    }
+
+
+// console.log("wecode" + 20);
+// console.log(true + "wecode");
+// console.log(30 + "wecode");
+
+
+//    let year = 2000;
+//    let century = Math.floor(year / 100) + 1;
+//    console.log(century);
+  
+
+
+//  let a = 50;
+//  let b = 20;
+//  let sum = 100;
+//  console.log(sum -(a + b));
+
+// for (let i = 1; i <=1; i++){
+//    console.log(i);
+//    console.log("hello")
+// }
+
+// let coffee = prompt("Do you drink coffee or tea? ");
+//  if (coffee){
+//     alert("i ")
+//  }else{
+//     alert("hello")
+//  }
+
+// let a = 15;
+// console.log(++a + 5 - a++ + 10 + --a - --a + ++a + ++a + a);
+// 16+5-16+10+16-15+16+17+17
+
+// let b = 5;
+// console.log(++b + 5 + b++ + 10 + b);
+
+
+// let c = 10;
+// console.log(++c);
+// console.log(c++);
+// console.log(c);
+
+
+
+
+// for(let i = 0; i<=100; i++){
+//     if(i%2==5){
+//         console.log(i)
+//     }else{
+//         console.log
+//  }
+// }
+// for(let i = 1; i<=100; i++){
+//     if(i%5===0){
+//         console.log(i)
+//     }
+// }
+// for(let i = 0; i<=100; i++){
+//     if(i%5===0 && i%15===0){
+//         console.log(i)
+//     }
+// }
+
+
+// let sum = 0;
+// for ( let i = 1; i <=7 ;i++){
+//     console.log(i,sum)
+//     sum  = sum + 1;
+// }
+
+// for(i = 50;i>=5;i--)
+// if(i%5===0){
+//     console.log(i)
+// }
+
+// let i = 1;
+// while(i <= 100 ){
+//     console.log(i);
+//     i++;
+// }
+
+let i = 3;
+while(i <= 51 ){
+    console.log(i);
+    i++;
+}
